87280265f07d6677fa1f889e3d5e71ee42f15316
nate13nate <nate13nate@gmail.com>
2020-10-23 22:49:55 -0500

    HomePage: Begin Home Page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Router, Route } from 'react-router-dom';
 import './index.css';
 
-import SamplePage1 from './pages/SamplePage1';
+import HomePage from './pages/HomePage';
 import SamplePage2 from './pages/SamplePage2';
 import Header from './components/Header';
 import history from './history';
@@ -10,11 +10,13 @@ import history from './history';
 class App extends React.Component {
   render() {
     return (
-      <Router history={history}>
-        <Header />
-        <Route path='/' component={SamplePage1} exact />
-        <Route path='/otherpage' component={SamplePage2} exact />
-      </Router>
+      <div className='App'>
+        <Router history={history}>
+          <Header />
+          <Route path='/' component={HomePage} exact />
+          <Route path='/otherpage' component={SamplePage2} exact />
+        </Router>
+      </div>
     );
   }
 }
