90458567bffa01086678283c81959e4000de3f3b
nate13nate <nate13nate@gmail.com>
2020-10-24 15:00:16 -0500

    Julien: Made some changes to login page

--- src/pages/LoginPage/index.js
+++ src/pages/LoginPage/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 
-import LoginField from '../../components/Login/Username';
-import Title from '../../components/Title';
-import Business from '../../classes/Business';
-
+import SearchBar from '../../components/SearchBar';
+import history from '../../history';
 
 import './index.css';
 
@@ -15,48 +13,34 @@ class LoginPage extends React.Component {
         username: "",
         password: "",
     };
-
-    this.updateUsername = this.updateUsername.bind(this);
-    this.updatePassword = this.updatePassword.bind(this);
-    this.login = this.login.bind(this);
-
   }
 
-  updateUsername(e) {
-    this.setState({username: e.target.value})
-
+  updateUsername = (username) => {
+    this.setState({ username })
   }
 
-  updatePassword(e) {
-    this.setState({password : e.target.value})
+  updatePassword = (password) => {
+    this.setState({ password })
   }
 
 
-  login(e) {
-    
+  login = (e) => {
+    e.preventDefault();
     //if incorrect password
     //alert("incorrect password")
 
     //login stuff
     alert("Username: " + this.state.username + " Password:" + this.state.password);
-    
-
+    history.push('/');
   }
-
-  
 
   render() {
     return (
       <div className='loginPage'>
-
-        <h1 className ="loginHeader"> Login </h1>
-        
-        <LoginField onChange = {this.updateUsername} title = "username" type ="text"/> 
-        <LoginField onChange = {this.updatePassword}title = "Password" type ="password"/> 
-        <button className ="loginButton" onClick = {this.login}> Login</button>
-        
+        <SearchBar value={this.state.username} sendValue={this.updateUsername} placeholder='Username'/> 
+        <SearchBar value={this.state.password} sendValue={this.updatePassword} placeholder='Password' /> 
+        <button className ="loginButton" onClick={this.login}>Submit</button>
       </div>
-      
     );
   }
 }
