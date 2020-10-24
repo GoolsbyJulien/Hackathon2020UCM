3bfc022a1d5cc17b3ebfb8b872bef6f98cd4f8a8
nate13nate <nate13nate@gmail.com>
2020-10-23 21:01:27 -0500

    Header: Made Basic Header

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import './index.css';
 
 import SamplePage1 from './pages/SamplePage1';
 import SamplePage2 from './pages/SamplePage2';
+import Header from './components/Header';
 import history from './history';
 
 class App extends React.Component {
   render() {
     return (
       <Router history={history}>
+        <Header />
         <Route path='/' component={SamplePage1} exact />
         <Route path='/otherpage' component={SamplePage2} exact />
       </Router>
